a8babe0fd382104cede61d7b925e94f6d157c946
Bohdan <s00ndie@icloud.com>
2024-08-20 21:08:51 +0200

    Homework 3.2

--- питон.py
+++ питон.py
@@ -1,17 +1,3 @@
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
-op = input("Choose operation '+,-,/,*': ")
-
-if op == "+":
-    print(num1+num2)
-elif op == '-':
-    print(num1-num2)
-elif op == '/':
-    if num2 > 0:
-        print(num1/num2)
-    else:
-        print('Second number should be more than 0')
-elif op == '*':
-    print(num1*num2)
-else:
-    print('Mistake')
+list = [1, 2, 3, 4, 5]
+last = [list[-1]]
+print(last + list[:-1])
